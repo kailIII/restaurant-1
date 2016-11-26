172e69a73911c17a725cec910de7c20ca24eda97
Bart <b4rtmail@gmail.com>
2016-11-26 22:40:00 +0100

    renamed a few files and update security default page

--- src/main/java/com/caramel/restaurant/security/SecurityConfig.java
+++ src/main/java/com/caramel/restaurant/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig
                 loginProcessingUrl("/applogin").
                 usernameParameter("username").
                 passwordParameter("password").
-                defaultSuccessUrl("/secure/admin")	
+                defaultSuccessUrl("/secure/reservation")	
         .and().logout()
         		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login.xhtml?error=logout")
@@ -84,23 +84,6 @@ public class SecurityConfig
 	    
 	    return driverManagerDataSource2;
 	}
-	
-	//query for postgresql
-//	    CREATE TABLE users
-//	    (
-//	      username character varying(50) NOT NULL,
-//	      "password" character varying(50) NOT NULL,
-//	      enabled boolean NOT NULL,
-//	      CONSTRAINT users_pkey PRIMARY KEY (username)
-//	    );
-//	    CREATE TABLE authorities
-//	    (
-//	      username character varying(50) NOT NULL,
-//	      authority character varying(50) NOT NULL,
-//	      CONSTRAINT fk_authorities_users FOREIGN KEY (username)
-//	          REFERENCES users (username) MATCH SIMPLE
-//	          ON UPDATE NO ACTION ON DELETE NO ACTION
-//	    );
 	
 	//query for mysql
 //	    create table users(
